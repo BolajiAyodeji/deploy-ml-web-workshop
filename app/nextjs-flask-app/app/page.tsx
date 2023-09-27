3d34de3f1cdfe06cc8030fd5f272757ce5f56eba
Bolaji Ayodeji <sirbeejay1@gmail.com>
2023-09-27 06:40:56 +0300

    docs: update lesson six

--- app/nextjs-flask-app/app/page.tsx
+++ app/nextjs-flask-app/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { FormEvent, useState } from "react";
 import Form from "./components/Form";
-import Result from "./components/result";
+import Result from "./components/Result";
 import { Footer } from "./components/Footer";
 
 type PredictionData = {
